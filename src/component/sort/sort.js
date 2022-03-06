48dfd01442e382ff4c07719dc8fd245cc50b25f4
kamlesh sharma <ksharma6876@gmail.com>
2022-03-06 22:05:08 +0530

    add current number visual bg

--- src/component/sort/sort.js
+++ src/component/sort/sort.js
@@ -6,9 +6,17 @@ const arr = [100, 95, 90, 85, 76, 70, 64, 55, 45, 35, 25, 15, 10, 5]
 const Sort = () => {
     const [arry, setArry] = useState(arr);
 
-    const printAfterTime = (sortedArry, time) => {
+    const printAfterTime = (sortedArry, time, currentNumber) => {
         setTimeout(() => {
-            setArry(sortedArry)
+            let allCurrentNum = document.querySelectorAll(`.bar`);
+            if( allCurrentNum.length ) {
+                allCurrentNum.forEach(element => {
+                    element.style.background = '#2B50AA';
+                });
+            }
+            setArry(sortedArry);
+            let currentNumObj = document.querySelector(`div[data-number="${currentNumber}"]`);
+            currentNumObj.style.background = "#ED461D";
         },time * 1000)
     }
 
@@ -18,9 +26,10 @@ const Sort = () => {
             for(let j=0; j<arry.length; j++){
                 if(arry[j] > arry[j+1]){
                     [arry[j], arry[j+1]] = [arry[j+1], arry[j]];
+                    // console.log(arry[j]);
                     const copyArry = [...arry]
                     seconds++
-                    printAfterTime(copyArry, seconds)
+                    printAfterTime(copyArry, seconds, arry[j+1])
                 }
             }
         }
@@ -33,7 +42,7 @@ const Sort = () => {
             <div className='sub-container' >
                 {arry.map((num, index) => {
                     return(<div key={index} className='bar-num'>
-                         <div className='bar' style={{height: `${num*5}px`}} ></div>
+                         <div className='bar' data-number={num} style={{height: `${num*5}px`}} ></div>
                          <p>{num}</p>
                     </div>)
                 } )}
